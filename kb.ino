a2f574f3b08238bc8a2bb646a866f6afa1fd6a6c
Mike Wu <mail@wumike.com>
2018-09-25 06:50:46 +0900

    Remove reading battery ADC and added comments

--- kb.ino
+++ kb.ino
@@ -40,74 +40,86 @@ void setup(void) {
 
 }
 
-uint8_t stop = 100;
 
-uint8_t last_reported = 999;
-uint32_t last_report_time = 0;
+
 void loop(void) {
-  if (!is_bluetooth_connected()) {
-    last_reported = 999;
-    last_report_time = 0;
-    analogWrite(LED, 128);
-    delay(500);
-    return;
-  }
-
-  float measuredvbat = analogRead(VBAT) * 6.6 / 1024;
-  uint8_t bat_percentage = (uint8_t)round((measuredvbat - 3.7) * 200);
-  if (
-      (last_reported != bat_percentage) && 
-      (millis() - last_report_time > 10000)
-  ) {
-    last_report_time = millis();
-    last_reported = bat_percentage;
-    update_battery(bat_percentage);
-  }
-
-  analogWrite(LED, 1 + (exp(sin(millis()/2000.0*PI)) - 0.36787944) * 107.0);
-
+  
   for (uint8_t row = 0; row < ROWS; row++) {
     uint8_t row_read = 0;
 
-    digitalWrite(row_pins[row], LOW);
+    digitalWrite(row_pins[row], LOW);                     // pull the row we're currently reading to LOW
+
+    // We set to LOW for the row we're reading because we're using INPUT_PULLUP on the column
+    // pins so they default to HIGH. If we didn't do this we'd have to wire pulldown 
+    // resistors to every switch!
+    
     delayMicroseconds(30);
 
-    for (uint8_t col = 0; col < COLS; col++) {
-      if (digitalRead(col_pins[col]) == LOW) {
-        row_read |= 1 << col;
+    for (uint8_t col = 0; col < COLS; col++) {            // for each column in the row
+      if (digitalRead(col_pins[col]) == LOW) {            // if column is LOW, means a key is pressed
+        row_read |= 1 << col;                             // shift left by column number first and then turn last binary digit into a 1
       }
     }
 
+    // if all 6 column pins are low (ie. all 6 keys in row are pressed)
+    // 1
+    // 11
+    // 111
+    // 1111
+    // 11111
+    // 111111
+
+    // if the temp state of the row isn't the same as what we read
     if (temp_states[row] != row_read) {
-      temp_states[row] = row_read;
-      debouncing = DEBOUNCING_DELAY;
+      temp_states[row] = row_read;          // set the temp state the read row state
+      debouncing = DEBOUNCING_DELAY;        // add a debouncing delay
     }
+    
     digitalWrite(row_pins[row], HIGH);
   }
 
+  // Basically, want the read row to not have changed for DEBOUNCING_DELAY amount
+  // of time until we process what we've read.
+
   if (debouncing) {
-    if (--debouncing) {
+    if (--debouncing) {     // decreases debouncing delay by 1
       delay(1);
+      // debouncing, don't update states             
     } else {
+
+      // read the row DEBOUNCING_DELAY number of times and it's still the same,
+      // so let's update our states
+      
       for (uint8_t row = 0; row < ROWS; row++) {
         prev_states[row] = curr_states[row];
         curr_states[row] = temp_states[row];
+
+        // temp is not the same as curr(ent) because we're debouncing
+        // to prevent multiple key switch registers
       }
     }
   }
 
+  // for each row
   for (uint8_t row = 0; row < ROWS; row++) {
+
     if (curr_states[row] == prev_states[row]) {
+      // no change in state
       continue;
     }
 
+    // there is a row change
+
     for (uint8_t col = 0; col < COLS; col++) {
-      state_t curr = (curr_states[row] >> col) & 1;
+      // shift right to look at current column, then bitwise & 1 which
+      // ignores columns to the left and checks to see if state is a
+      // 1 or 0.
+      state_t curr = (curr_states[row] >> col) & 1;       
       state_t prev = (prev_states[row] >> col) & 1;
       if (curr != prev) {
         handle_keychange(row >> 1, 2 * col + (row & 1), curr);
         prev_states[row] ^= (uint16_t)1 << col;
-        goto END_OF_LOOP;
+        goto END_OF_LOOP;   // only handle 1 key change each iteration, debouncing!
       }
     }
   }
--- keymap.h
+++ keymap.h
@@ -2,8 +2,8 @@
 #define KEYMAP_H
 
 #define state_t bool
-#define UP false
-#define DOWN true
+#define UP false        // LOW = 0 = FALSE = KEY UP
+#define DOWN true       // HIGH = 1 = TRUE = KEY DOWN
 
 void handle_keychange(uint8_t row, uint8_t col, state_t state);
 
